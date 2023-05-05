2b084ec45e35f63342a07c3ddb6c1813ddee6ef8
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2023-05-05 22:47:43 +0700

    added refresh token

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,31 @@ import { INestApplication } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { API_PREFIX, PORT } from './shared/constants';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
   app.setGlobalPrefix(API_PREFIX);
+  app.use(cookieParser());
+
+  app.use(
+      session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+      }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   initSwagger(app);  
 
